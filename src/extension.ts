beb58882e66fb1b9a1080719af08016de884050a
Ian Hornik <yiliansource@gmail.com>
2021-05-04 14:36:32 +0200

    Set up basic extension

---
+++ src/extension.ts
@@ -0,0 +1,7 @@
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+    console.log('Loaded "vscode-pastemyst".');
+}
+
+export function deactivate() {}
