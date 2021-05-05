107cb48b23dd9c4a5bf4f40a00107401fdabcc2a
Ian Hornik <yiliansource@gmail.com>
2021-05-05 17:31:19 +0200

    Added API token to config

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import * as pastemyst from "pastemyst-ts";
 
 import { registerCommands } from "./commands/register";
 import { registerProviders } from "./providers/register";
@@ -7,6 +8,12 @@ import languageStore from "./data/languageStore";
 import pasteStore from "./data/pasteStore";
 
 export function activate(context: vscode.ExtensionContext) {
+    const config = vscode.workspace.getConfiguration("pastemyst");
+
+    // Authorize requests using the API.
+    const token = config.get<string>("apiToken");
+    token && pastemyst.authorize(token);
+
     // Register the commands and providers and acknowledge their subscriptions.
     context.subscriptions.push(...registerCommands(), ...registerProviders());
 }
