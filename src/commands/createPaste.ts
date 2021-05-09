a59dea4209e993d4f0dd5f5cf5a205606f211b6f
mellobacon <42365887+mellobacon@users.noreply.github.com>
2021-05-08 23:51:49 -0500

    added paste creation functionality

--- src/commands/createPaste.ts
+++ src/commands/createPaste.ts
@@ -1,5 +1,6 @@
 import * as vscode from "vscode";
 import * as pastemyst from "pastemyst-ts";
+import * as open from "open";
 
 export default async function createPaste(): Promise<void>{
     const files = vscode.workspace.textDocuments;
@@ -15,10 +16,32 @@ export default async function createPaste(): Promise<void>{
     await vscode.window.showQuickPick([...items] , {
         placeHolder: "Select files to send to PasteMyst",
         canPickMany: true
-    });   
-
-    // placeholder. will get back to this later
-    let pastes = await pastemyst.pastes.createPaste({
-        pasties: [{title: "e", language: "", code: "e"}]
     });
+    
+    // make a pasty for each file
+    let pastes : Omit<pastemyst.Pasty, "_id">[] = [];
+    files.forEach(file => {
+        let filename = file.fileName.split("\\");
+        let title = filename[filename.length - 1];
+        let lang = "autodetect"; // something something temporary
+        let pst : Omit<pastemyst.Pasty, "_id"> = {title: title, language: lang, code: file.getText()};
+        pastes.push(pst);
+    });
+
+    // Create a new paste of pasties
+    let paste = await pastemyst.pastes.createPaste({
+        pasties: [...pastes]
+    });
+
+    // Make sure there is no error in making a paste
+    if (!paste){
+        vscode.window.showErrorMessage("Error: Cannot create paste.");
+        return;
+    }
+    
+    // Send ye to the pastemyst link
+    let uri = `https://paste.myst.rs/${paste._id}`;
+    vscode.window
+    .showInformationMessage("Paste successfully created at: " + uri + "!", "Open page")
+    .then(() => {open(uri);});
 }
