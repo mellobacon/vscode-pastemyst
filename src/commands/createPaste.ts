55e1559e02434f53c63478d97d559afbc2b12fde
mellobacon <liz6757@outlook.com>
2021-05-09 17:12:44 -0500

    stuff

--- src/commands/createPaste.ts
+++ src/commands/createPaste.ts
@@ -6,42 +6,51 @@ export default async function createPaste(): Promise<void>{
     const files = vscode.workspace.textDocuments;
 
     // Collect all workspace files as a quickpick item
-    var items : vscode.QuickPickItem[] = [];
+    let items : vscode.QuickPickItem[] = [];
     files.forEach(ele => {
         let item : vscode.QuickPickItem = {label: ele.fileName, description: ele.languageId};
         items.push(item);
     });
 
     // Show the list of files in the workspace
-    await vscode.window.showQuickPick([...items] , {
-        placeHolder: "Select files to send to PasteMyst",
-        canPickMany: true
+    let p = vscode.window.createQuickPick();
+    p.placeholder = "Select files to send to PasteMyst";
+    p.items = [...items];
+    p.canSelectMany = true;
+    p.show();
+    p.onDidAccept(() => {
+        sendPaste();
+        p.hide();
     });
     
-    // make a pasty for each file
-    let pastes : Omit<pastemyst.Pasty, "_id">[] = [];
-    files.forEach(file => {
-        let filename = file.fileName.split("\\");
-        let title = filename[filename.length - 1];
-        let lang = "autodetect"; // something something temporary
-        let pst : Omit<pastemyst.Pasty, "_id"> = {title: title, language: lang, code: file.getText()};
-        pastes.push(pst);
-    });
+    pastemyst.authorize("SuEYQoR8bn0bHMjZj5WqEl/8ehZvIvpE0vue11kEmHY=");
+    function sendPaste(){
+        // Make a pasty for each file
+        let pastes : Omit<pastemyst.Pasty, "_id">[] = [];
+        files.forEach(file => {
+            let filename = file.fileName.split("\\");
+            let title = filename[filename.length - 1];
+            let lang = "autodetect"; // something something temporary
+            let pst : Omit<pastemyst.Pasty, "_id"> = {title: title, language: lang, code: file.getText()};
+            pastes.push(pst);
+        });
 
-    // Create a new paste of pasties
-    let paste = await pastemyst.pastes.createPaste({
-        pasties: [...pastes]
-    });
-
-    // Make sure there is no error in making a paste
-    if (!paste){
-        vscode.window.showErrorMessage("Error: Cannot create paste.");
-        return;
+        // Create a new paste of pasties
+        let paste = pastemyst.pastes.createPaste({
+            isPrivate: true,
+            pasties: [...pastes]
+        }).then(p => {
+            // Make sure there is no error in making a paste
+            if (!paste){
+                vscode.window.showErrorMessage("Error: Cannot create paste.");
+                return;
+            }
+            
+            // Send ye to the pastemyst link
+            let uri = `https://paste.myst.rs/${p?._id}`;
+            vscode.window
+            .showInformationMessage("Paste successfully created at: " + uri + "!", "Open page")
+            .then(() => {open(uri);});
+            });
     }
-    
-    // Send ye to the pastemyst link
-    let uri = `https://paste.myst.rs/${paste._id}`;
-    vscode.window
-    .showInformationMessage("Paste successfully created at: " + uri + "!", "Open page")
-    .then(() => {open(uri);});
 }
